c584d2085fd04af5026a90b48ec988a7e7de4e65
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-10-03 20:33:00 +0300

    Backend <> jwt : implement jwt

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(["prefix"=> "v0.1"], function(){
+   
+    Route::group(["middleware" => "auth:api"], function(){
+        Route::get("/logout", [AuthController::class, "logout"])->name("landing-user");
+        Route::post("/store/{id?}", [LandingController::class, "addOrUpdateStore"])->name("add-user"); 
+        Route::get("/categories/{id?}", [LandingController::class, "getCategories"])->name("landing-user");
+   });
+   Route::post("/register", [AuthController::class, "register"])->name("register");
+   Route::post("/login", [AuthController::class, "login"])->name("login");
+   Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
