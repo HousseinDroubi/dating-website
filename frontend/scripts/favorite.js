4bc64882bc0eddea2c692b1cd76b587783c280d6
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-10-04 20:05:07 +0300

    Frontend <> favorite : add comments

--- frontend/scripts/favorite.js
+++ frontend/scripts/favorite.js
@@ -1,22 +1,27 @@
+// The whole_users is the parent container that we will append child every iteration on it
 const whole_users = document.getElementById("whole_users");
 const url_favorites='http://127.0.0.1:8000/api/v0.1/favorites';
 
+// Get all favorites users to this user
 const getFavorites = async () =>{
     const api_data = {'token':localStorage.getItem("token"),
-                'favoriting':localStorage.getItem("id"),
-               };
+                      'favoriting':localStorage.getItem("id"),
+                     };
     try{
         await axios.post(
         url_favorites,
         api_data,
         ).then((response)=>{
+    // number_user is the length of the array that contains favorite list.         
     const number_user = response.data.data.length;
+    // array_users is the array that will contain all data about users in favorite list.  
     const array_users= response.data.data;      
     for (let i=0;i<number_user;i++){
-        // console.log(array_users[i][0].email);
+        // user is the container that will we use to appench small contents to it
         const user =document.createElement('div');
         user.classList.add('user');
 
+        // Now, we are adding all the containers of 'user' and their classes
         const user_profile_image = document.createElement('div');
         user_profile_image.classList.add('user-profile-image');
         const img = document.createElement('img');
@@ -46,12 +51,16 @@ const getFavorites = async () =>{
         const h5 = document.createElement('h5');
         user_third_child.appendChild(h5);
 
+        // Now, we are adding these small container to 'user'
         user.appendChild(user_profile_image);
         user.appendChild(user_first_child);
         user.appendChild(user_second_child);
         user.appendChild(user_third_child);
-             
+        
+        // user_details is the user detail of each.
         const user_details = array_users[i][0];
+        
+        // Here, we are getting each single data we might use in order to adapt it on a content.
         const id = user_details.id;
         const username = user_details.username;
         const img_src = user_details.image;
@@ -64,50 +73,47 @@ const getFavorites = async () =>{
         const age = user_details.age;
         const bio = user_details.bio;
 
+        // Here, we are adapting the above data on the below contents.
         img.src=img_src;
         h3.innerText = username;
         h3_age.innerText = age;
         h3_gender.innerText = gender;
         h5.innerText = bio;
 
+        // Put a text and background for heart and button.
         heart.innerHTML =`&#10084;&#65039;`;
         button.innerText = 'Message';
 
+        // The below funtion will get the id and username of a specific user and redirect him to conversation
+        // page. 
         const goChat = () =>{
             localStorage.setItem('chat_with_id',id);
             localStorage.setItem('chat_with_username',username);
             window.location.href = './conversation.html';
         }
+
+        // The below funtion will remove a specific user from the favorite list of the registered list.
         const unlike = async() =>{
             
-
-
             const url_remove_favorite='http://127.0.0.1:8000/api/v0.1/favorite/remove';
 
-                const api_data_remove_favorite = {'token':localStorage.getItem("token"),
-                            'favoriting':localStorage.getItem("id"),
-                            'favorited':id,
-                           };
+            const api_data_remove_favorite = {'token':localStorage.getItem("token"),
+                        'favoriting':localStorage.getItem("id"),
+                        'favorited':id,
+                        };
                 try{
                     await axios.post(
                         url_remove_favorite,
                         api_data_remove_favorite,
                     ).then((response)=>{
                         if(response.data.status=='deleted'){
+                            // If the status was 'deleted', the page will be refreshed.
                             location. reload();
                         }
                     });
                 }catch(error){
                     window.location.href = './login.html';
                 }
-            
-
-
-
-
-
-
-
 
         }
         heart.addEventListener('click',unlike);
@@ -122,23 +128,3 @@ const getFavorites = async () =>{
     }
 }
 getFavorites();
-// const url_me='http://127.0.0.1:8000/api/v0.1/favorites';
-
-// const getProfile = async () =>{
-//     api_data = {'favoriting':'1',
-//                 'favorited':'2',
-//                };
-//     try{
-//         await axios.post(
-//         url_me,
-//         api_data,
-//         ).then((response)=>{
-//             s = response.data.data[0];
-//             console.log(s[0].email);
-//         });
-//     }catch(error){
-//         // window.location.href = './login.html';
-//         console.log(error);
-//     }
-// }
-// getProfile();
